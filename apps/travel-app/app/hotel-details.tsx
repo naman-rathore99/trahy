ebb35f88fb36907b01b297e826e6e35fe0443ca3
Naman Rathore <naman.rathore59@gmail.com>
2026-01-17 22:50:20 +0530

    change of app. api intregation remaining

--- apps/travel-app/app/hotel-details.tsx
+++
@@ -1,4 +0,0 @@
-// This file acts as the 'Bridge' between Expo Router and your Screen code
-import HotelDetailsScreen from '../src/screens/HotelDetailsScreen';
-
-export default HotelDetailsScreen;
--- apps/travel-app/app/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Redirect } from 'expo-router';
-import '../global.css';
-export default function Index() {
-  // As soon as the app opens, go to the Home Tab
-  return <Redirect href="./(tabs)/home" />;
-}
